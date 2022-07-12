1dfa07cf3f26a2b56316f13af2afa41f95462680
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-12 20:53:47 +0530

    Add files via upload

---
+++ prac_File_Handling.py
@@ -0,0 +1,19 @@
+# a=open("sindhu.txt",'x')
+# a=open("bindu.txt",'w')
+# a.write("hey python")
+# a.close
+# b=open("bindu.txt",'a')
+# b.write("\nhow are you")
+# b.close
+# c=open("bindu.txt",'r')
+# print(c.read())
+
+# import os
+# print(os.getcwd())
+
+# import os
+# os.mkdir("batch2pm")
+# os.rmdir("batch2pm")
+# os.remove("sindhu.txt")
+# print(os.path.exists("bindu.txt"))
+# print(os.path.exists("sindu.txt"))
